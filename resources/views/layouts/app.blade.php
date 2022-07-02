387a64ef40d7296eb0e49b06e711c8e6a3d105e0
Eddie Santos <62967219+Eddiesantle@users.noreply.github.com>
2022-07-01 21:20:51 -0300

    hotjan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -15,7 +15,7 @@
     <meta name="keywords" content="streamer, games, team, scum, torneio, rpg, recrutar" />
     <meta name="revised" content="15/04/2022" />
     <meta name="google-site-verification" content="ACphy9ljwZokf3teYxBC-A0FUhFoZh1z_LGH2SVNKIQ" />
-    
+
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -28,6 +28,19 @@
     gtag('config', 'G-C32BJ91254');
     </script>
 
+
+    <!-- Hotjar Tracking Code for https://stsemtag.com.br/ -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3048710,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
     <!-- Styles -->
     <link rel="preload" href="{{ asset('assets/css/style.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link href="{{ asset('assets/css/style.css') }}" rel="stylesheet"></noscript>
@@ -37,14 +50,14 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="preload" href="https://fonts.googleapis.com/css?family=Nunito" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"></noscript>
-    
 
-    
+
+
 </head>
 <body>
     <div id="app">
-        
-        
+
+
           <header class="sticky-top">
             <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 
@@ -63,7 +76,7 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('home.sobre') }}">SOBRE</a>
                             </li>
-                            
+
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('home.recrutamento') }}">RECRUTAMENTO</a>
                             </li>
@@ -78,15 +91,15 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="#contato">CONTATO</a>
                             </li>
-                            
+
                             <li class="nav-item dropdown d-flex">
-    
+
                                 @if(Auth::check() )
-                                
+
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" role="button" data-toggle="dropdown" aria-expanded="false">
 
                                         <img src="{{ Auth::user()->avatar}}" alt="avatar {{ Auth::user()->username }}" style="width: 30px;border-radius:50px;" loading="lazy">
-                                        
+
                                         Olá, {{ Auth::user()->username }}
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -95,18 +108,18 @@
                                         <li><a class="dropdown-item" href="{{ route('admin.index') }}">Area restrita</a></li>
                                         @endif
                                         <li><a class="dropdown-item" href="{{ route('logout') }}">Sair</a></li>
-                                        
+
                                     </ul>
-    
-                                    
-            
+
+
+
                                 @else
                                     <a href="{{ route('auth.steam') }}" class="nav-link"><img src="{{ asset('assets/img/login.png') }}" alt="fazer login" loading="lazy" class="pr-2" style="width: 25px"> Fazer login</a>
                                 @endif
                             </li>
                         </ul>
                     </div>
-               
+
             </nav>
         </header>
 
@@ -119,14 +132,14 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-md-12" style="margin-top: 5%;height:400px;">
-             
+
                             <div class="mx-auto" style="width: 300px">
                                 <img src="{{ asset('assets/img/game-06.png') }}" alt="" loading="lazy" class="float-left" style="width: 70px">
                                 <h4 class="text-white text-center font-weight-bold">FAÇA PARTE DA NOSSA DOAÇÃO:</h4>
                             </div>
                             <p class="text-white text-center mx-auto py-4" style="max-width: 500px">Conectamos gamers que tem equipamentos para doar, com outros gamers que necessitam de um equipamento. Colabore com o ST!
                             </p>
-            
+
                             <div class="mx-auto text-center" style="width: 325px">
                                 <h4 class="text-white">QUER COLABORAR E AJUDAR OUTROS GAMERS?</h4>
                                 <a class="btn px-4 my-4 btn-principal text-dark font-weight-bold"  href="{{ route('home.contribuir') }}" style="border-radius:10px;">DOAR EQUIPAMENTO</a>
@@ -146,7 +159,7 @@
                             </div> --}}
                         </div>
                     </div>
-            
+
                 </div>
             </div>
             <div class="container-fluid">
@@ -155,7 +168,7 @@
                         <div class="w-100 p-5">
                             <h3 class="font-weight-bold text-white mt-4">YOUTUBE</h3>
                             <p class="text-principal font-weight-bold">Canal</p>
-    
+
                             <button class="btn px-4 my-3" type="button" style="color:#fff;border-radius:10px;border:1 solid #fff;border-color:#fff;">Ver mais</button>
                         </div>
                     </div>
@@ -163,7 +176,7 @@
                         <div class="w-100 p-5">
                             <h3 class="font-weight-bold text-white mt-4">TWITCH</h3>
                             <p class="text-principal font-weight-bold">Stream</p>
-    
+
                             <button class="btn px-4 my-3" type="button" style="color:#fff;border-radius:10px;border:1 solid #fff;border-color:#fff;">Ver mais</button>
                         </div>
                     </div>
@@ -171,60 +184,60 @@
                         <div class="w-100 p-5">
                             <h3 class="font-weight-bold text-white mt-4">INSTAGRAM</h3>
                             <p class="text-principal font-weight-bold">Rede social</p>
-    
+
                             <button class="btn px-4 my-3" type="button" style="color:#fff;border-radius:10px;border:1 solid #fff;border-color:#fff;">Ver mais</button>
                         </div>
                     </div>
-    
+
                 </div>
             </div>
             <div class="container" id="contato">
-           
+
                 <h4 class="text-center py-5"><b>ENTRE EM CONTATO</b></h4>
-        
+
                 <div class="row justify-content-center pb-5">
                     <div class="col-10 col-md-5 img-fundo-contato" style="border-radius:5px;">
                         <h5 class="text-white text-center w-75 mx-auto" style="margin-top:40%;margin-bottom:20%;"><b>Deixe sua mensagem que em breve nossa equipe retornará!</b></h5>
                     </div>
                     <div class="col-10 col-md-5" style="background-color:#1F0037; border-radius:5px; box-shadow: 5px 5px 2em black;">
-                    
+
                         <p class="text-white text-center w-75 mx-auto my-3" ><b>Preencha o formulário</b></p>
 
                         <form class="text-center p-3" method="POST" action="{{ route('email.contato') }}">
                             @csrf
                             <div class="form-group">
-                              
+
                               <input type="text" class="icon-avatar form-person bg-segundaria" id="nomeContato" name="nomeContato" placeholder="Seu nome" required>
-                              
+
                             </div>
                             <div class="form-group">
-                              
+
                                 <input type="text" class="icon-send form-person bg-segundaria" id="emailContato" name="emailContato" placeholder="Email" required>
-                                
+
                             </div>
                             <div class="form-group">
-                              
+
                                 <input type="text" class="icon-send form-person bg-segundaria" id="assuntoContato" name="assuntoContato" placeholder="Assunto">
-                                
+
                             </div>
                             <div class="form-group">
-                              
+
                                 <textarea class="form-person bg-segundaria" id="mensagemContato" name="mensagemContato" rows="3" placeholder="Escreva sua mensagem"></textarea>
-                                
+
                             </div>
 
                             <button type="submit" class="btn btn-principal px-4 text-dark font-weight-bold">Enviar</button>
                         </form>
 
 
-                        
-                        
-          
-                    
+
+
+
+
                     </div>
                 </div>
-                
-    
+
+
             </div>
 
             @if (count($patrocinadores) != 0)
@@ -242,7 +255,7 @@
                                  <div class="border w-100 my-3"></div>
                             </div>
                         </div>
-                
+
                         <div class="row justify-content-center pb-4">
                             @foreach ($patrocinadores as $patrocinio)
                                 <div class="col-7 col-md-3 my-3">
@@ -254,7 +267,7 @@
                                 </div>
                             @endforeach
                         </div>
-                
+
                     </div>
                 </div>
             </div>
@@ -275,7 +288,7 @@
                         </div>
                         <div class="col-md-4 col-lg-4 mb-2">
                             <div class="w-100 text-center">
-                                
+
                                 <p class="text-white font-weight-bold my-3">MENU DO SITE</p>
                                 <div class="row pt-3">
                                     <div class="col-6">
@@ -286,7 +299,7 @@
                                         <div class="border mx-auto w-75 my-3"></div>
 
                                         <p><a href="{{ route('home.recrutamento') }}" class="text-white" style="text-decoration:none">RECRUTAMENTO</a></p>
-                                        
+
                                     </div>
                                     <div class="col-6">
                                         <p><a href="{{ route('home.torneios') }}" class="text-white" style="text-decoration:none">TORNEIO</a></p>
@@ -296,12 +309,12 @@
                                         <div class="border mx-auto w-75 my-3"></div>
 
                                         <p><a href="{{ route('home.contribuir') }}" class="text-white" style="text-decoration:none">CONTRIBUA</a></p>
-                                        
+
                                     </div>
                                 </div>
                             </div>
                         </div>
-            
+
                         <div class="col-md-4 col-lg-3 mb-2">
                             <div class="w-100">
                                 <p class="text-white font-weight-bold my-3">CONTATO</p>
